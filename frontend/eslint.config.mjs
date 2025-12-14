fb084a90578f874d1dd70d77515111b4a116f238
feril <ferilferdian4@gmail.com>
2025-12-15 01:36:51 +0700

    remove airbnb eslint and use eslint default for nextjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,43 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    'airbnb',
-    'airbnb/hooks',
-    'next/core-web-vitals',
-    'next/typescript'
-  ),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
     ],
-  },
-  {
-    rules: {
-      'no-underscore-dangle': 'off',
-      'linebreak-style': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'quotes': 'off', // Allow both single and double quotes
-      'import/extensions': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react/jsx-filename-extension': 'off',
-      'react/button-has-type': 'off',
-      'no-console': 'off',
-      'no-restricted-globals': 'off',
-      'no-alert': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'react/no-array-index-key': 'warn',
-      'no-nested-ternary': 'warn',
-      'react/jsx-one-expression-per-line': 'off',
-      'implicit-arrow-linebreak': 'off',
-      'object-curly-newline': 'off',
-      'react/jsx-curly-newline': 'off',
-      'function-paren-newline': 'off',
-    },
   },
 ];
 
